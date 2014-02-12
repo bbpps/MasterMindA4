b83b1504b38a036714ce308adcf50ef900cd1f6d
ahood12 <adamhood12@gmail.com>
2014-02-12 01:37:30 -0600

    Update Code.java

--- Code.java
+++ Code.java
@@ -4,36 +4,36 @@ public class Code {
 	private Peg[] elements;
 	private int bCount, wCount;
 
-	public Code(){
+	public Code() {
 		elements = new Peg[4];
 		bCount = 0;
 		wCount = 0;
 	}
 
-	//gets the Peg at position "index"
-	public Peg get(int index){
+	// gets the Peg at position "index"
+	public Peg get(int index) {
 		return elements[index];
 	}
 
-	//returns the size of the Code
-	public int getSize(){
+	// returns the size of the Code
+	public int getSize() {
 		return elements.length;
 	}
 
-	//getters for the counters
-	public int getBlackCount(){
+	// getters for the counters
+	public int getBlackCount() {
 		return bCount;
 	}
 
-	public int getWhiteCount(){
+	public int getWhiteCount() {
 		return wCount;
 	}
 
-	//converts a string of the Color chars (BGOPRY)
-	//into Pegs which is then stored into elements
-	public void convertString(String code){
-		for(int c=0; c<code.length(); c++){
-			switch(code.toUpperCase().charAt(c)){
+	// converts a string of the Color chars (BGOPRY)
+	// into Pegs which is then stored into elements
+	public void convertString(String code) {
+		for (int c = 0; c < code.length(); c++) {
+			switch (code.toUpperCase().charAt(c)) {
 			case 'B':
 				elements[c] = new Peg(Colors.BLUE);
 				break;
@@ -56,30 +56,30 @@ public class Code {
 		}
 	}
 
-	//converts the code from an array of pegs to a string
-	public String convertCodetoString(){
+	// converts the code from an array of pegs to a string
+	public String convertCodetoString() {
 		String result = "";
-		for(int c=0; c<elements.length; c++){
-			if(elements[c].getColor().equals(Colors.BLUE))
-				result+= "B";
-			else if(elements[c].getColor().equals(Colors.GREEN))
-				result+="G";
-			else if(elements[c].getColor().equals(Colors.ORANGE))
-				result+="O";
-			else if(elements[c].getColor().equals(Colors.PURPLE))
-				result+="P";
-			else if(elements[c].getColor().equals(Colors.RED))
-				result+="R";
-			else if(elements[c].getColor().equals(Colors.YELLOW))
-				result+="Y";
+		for (int c = 0; c < elements.length; c++) {
+			if (elements[c].getColor().equals(Colors.BLUE))
+				result += "B";
+			else if (elements[c].getColor().equals(Colors.GREEN))
+				result += "G";
+			else if (elements[c].getColor().equals(Colors.ORANGE))
+				result += "O";
+			else if (elements[c].getColor().equals(Colors.PURPLE))
+				result += "P";
+			else if (elements[c].getColor().equals(Colors.RED))
+				result += "R";
+			else if (elements[c].getColor().equals(Colors.YELLOW))
+				result += "Y";
 		}
 		return result;
 	}
-	
-	//checks if the current peg is within elements
+
+	// checks if the current peg is within elements
 	public boolean contains(Peg peg) {
-		for(int i=0; i<elements.length; i++){
-			if(peg.equals(elements[i]))
+		for (int i = 0; i < elements.length; i++) {
+			if (peg.equals(elements[i]))
 				return true;
 		}
 		return false;
@@ -87,15 +87,15 @@ public class Code {
 
 	@Override
 	public boolean equals(Object other) {
-		if(other == null)
+		if (other == null)
 			return false;
-		else if(this.getClass() != other.getClass())
+		else if (this.getClass() != other.getClass())
 			return false;
-		else{
-			Code otherCode = (Code)other;
-			//check if elements are the same
-			for(int codeIndex = 0; codeIndex < this.getSize(); codeIndex++){
-				if(!this.get(codeIndex).equals(otherCode.get(codeIndex))){
+		else {
+			Code otherCode = (Code) other;
+			// check if elements are the same
+			for (int codeIndex = 0; codeIndex < this.getSize(); codeIndex++) {
+				if (!this.get(codeIndex).equals(otherCode.get(codeIndex))) {
 					return false;
 				}
 			}
@@ -103,25 +103,28 @@ public class Code {
 		return true;
 	}
 
-	//increments the black counter for this Code instance
+	// increments the black counter for this Code instance
 	public void incrementBlack() {
-		bCount++;		
+		bCount++;
 	}
 
-	//increments the white counter for this Code instance
+	// increments the white counter for this Code instance
 	public void incrementWhite() {
 		wCount++;
 	}
-	
-	//sets the element at pos to peg
-	public void set(int pos, Peg peg){
+
+	// sets the element at pos to peg
+	public void set(int pos, Peg peg) {
 		elements[pos] = peg;
 	}
 
-	public String toString(){
+	public String toString() {
 		StringBuilder sb = new StringBuilder();
-		for(int codeIndex = 0; codeIndex < elements.length; codeIndex++){
-			sb.append(elements[codeIndex].toString() + "");
+		for (int codeIndex = 0; codeIndex < elements.length; codeIndex++) {
+			if (elements[codeIndex] == null)
+				sb.append(".");
+			else
+				sb.append(elements[codeIndex].toString() + "");
 		}
 		return sb.toString();
 	}
